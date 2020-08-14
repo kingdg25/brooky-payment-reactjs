47e7aa6c60446155685fd5dde3a74fd9f5214dcb
kingiedg <kingiedg@gmail.com>
2020-08-14 14:23:40 +0800

    sh

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-git reset --hard
-git checkout PaymentForm
-git pull
-yarn install
-yarn run check
-rm -rf build
-yarn build
-firebase deploy --only hosting:checkout --token 1//0eGWzPL-SIiPzCgYIARAAGA4SNwF-L9Ir6f8x97CqZL2s-LCiST_gKiaZk8ii7eVuPJ2x6rX2JPoqujbWNNAylbfPaPYLQbzudzo
 
+rm -rf build
+npm run build
+firebase deploy --only hosting
