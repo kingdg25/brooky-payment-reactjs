d780d9a12ff43dd70c8bd4ab3efe27544966f5e3
kingiedg <kingiedg@gmail.com>
2020-08-22 15:45:44 +0800

    unit info

--- src/app/pages/checkout/views/Header.jsx
+++ src/app/pages/checkout/views/Header.jsx
@@ -7,7 +7,7 @@ import Container from "@material-ui/core/Container"
 import {
     Divider
 } from '@material-ui/core'
-import { makeStyles, withStyles } from '@material-ui/core'
+import { withStyles } from '@material-ui/core'
 
 const paymentDict = {
     credit: "Credit/Debit Card",
@@ -94,8 +94,8 @@ class Header extends Component {
                     style={{ backgroundColor: "white", color: "black", padding: "8px" }}
                 >
                     <Container maxWidth={"md"} disableGutters style={{ padding: "8px" }}>
-                        <Grid item md={12} spacing={2} direction="row" item container>
-                            <Grid md={6} direction="row" item container>
+                        <Grid item md={12} spacing={2} direction="row" container>
+                            <Grid md={6} direction="row" alignContent="flex-start" alignItems="flex-start" item container>
                                 <Grid md={12} spacing={3} direction="row" item container>
                                     <Grid item md={6}>
                                         <Typography
@@ -149,25 +149,112 @@ class Header extends Component {
                                 <Grid md={12} spacing={3} direction="row" item container>
                                     <Grid item md={6}>
                                         <Typography
-                                            style={{ textAlign: "start", color: "lightslategray" }}
-                                            variant="subtitle2"
+                                            style={{ textAlign: "start", color: "#4b4b4b" }}
+                                            variant="subtitle1"
                                             gutterBottom
                                         >
                                             <b>Buyer Details</b>
                                         </Typography>
                                     </Grid>
                                     <Grid item md={6}>
+                                        <Grid item md={12}>
+                                            <Typography
+                                                style={{
+                                                    textAlign: window.innerWidth > 450 ? "start" : "center",
+                                                    color: "#707070",
+                                                    fontWeight: "bold",
+                                                }}
+                                                variant="body1"
+                                                gutterBottom
+                                            >
+                                                {this.props.data.buyers_first_name} {this.props.data.buyers_last_name}
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item md={12}>
+                                            <Typography
+                                                style={{
+                                                    textAlign: window.innerWidth > 450 ? "start" : "center",
+                                                    color: "#707070",
+                                                }}
+                                                variant="subtitle1"
+                                                gutterBottom
+                                            >
+                                                {this.props.data.buyers_email}
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item md={12}>
+                                            <Typography
+                                                style={{
+                                                    textAlign: window.innerWidth > 450 ? "start" : "center",
+                                                    color: "#707070",
+                                                }}
+                                                variant="subtitle2"
+                                                gutterBottom
+                                            >
+                                                {this.props.data.buyers_mobile_number}
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                                <Grid md={12} spacing={3} direction="row" item container>
+                                    <Grid item md={6}>
                                         <Typography
-                                            style={{
-                                                textAlign: window.innerWidth > 450 ? "start" : "center",
-                                                color: "#2680EB",
-                                                fontWeight: "bold",
-                                            }}
-                                            variant="subtitle2"
+                                            style={{ textAlign: "start", color: "#4b4b4b" }}
+                                            variant="subtitle1"
                                             gutterBottom
                                         >
-                                            {this.props.transactionID}
+                                            <b>Unit Details</b>
                                         </Typography>
+                                    </Grid>
+                                    <Grid item md={6}>
+                                        <Grid item md={12}>
+                                            <Typography
+                                                style={{
+                                                    textAlign: window.innerWidth > 450 ? "start" : "center",
+                                                    color: "#707070",
+                                                }}
+                                                variant="subtitle2"
+                                                gutterBottom
+                                            >
+                                                <b>Project:</b> {this.props.data.project}
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item md={12}>
+                                            <Typography
+                                                style={{
+                                                    textAlign: window.innerWidth > 450 ? "start" : "center",
+                                                    color: "#707070",
+                                                }}
+                                                variant="subtitle2"
+                                                gutterBottom
+                                            >
+                                                <b>Phase/Condo/Tower:</b> {this.props.data.phase}
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item md={12}>
+                                            <Typography
+                                                style={{
+                                                    textAlign: window.innerWidth > 450 ? "start" : "center",
+                                                    color: "#707070",
+                                                }}
+                                                variant="subtitle2"
+                                                gutterBottom
+                                            >
+                                                <b>Block/Floor:</b> {this.props.data.block}
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item md={12}>
+                                            <Typography
+                                                style={{
+                                                    textAlign: window.innerWidth > 450 ? "start" : "center",
+                                                    color: "#707070",
+                                                }}
+                                                variant="subtitle2"
+                                                gutterBottom
+                                            >
+                                                <b>Lot/Unit:</b> {this.props.data.lot}
+                                            </Typography>
+                                        </Grid>
                                     </Grid>
                                 </Grid>
                             </Grid>
